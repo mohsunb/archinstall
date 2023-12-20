98564cecc5513a1fb69ea253a9a1f3034a9089e6
Möhsün Babayev <mohsun.babayev@protonmail.com>
2023-12-20 14:33:59 +0400

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/zsh
 
+printf "Enter disk name: "
+read BLOCK_DEVICE
+printf "Disk: '$BLOCK_DEVICE\n'"
+
+printf "Enter 'Confirm' to continue: "
+read CONTINUE
+
+if [[ $CONTINUE != "Continue" ]]; then
+	printf "Aborting...\n"
+	exit 1
+fi
+
+if [[ $BLOCK_DEVICE == *"nvme"* ]]; then
+	BLOCK_SUB="${BLOCK_DEVICE}p"
+else
+	BLOCK_SUB="$BLOCK_DEVICE"
+fi
+
 timedatectl set-ntp true
 
-wipefs --all /dev/nvme0n1
-printf "g\nn\n\n\n+1G\nt\n1\nn\n\n\n\nw\n" | fdisk /dev/nvme0n1
+wipefs --all $BLOCK_DEVICE
+printf "g\nn\n\n\n+1G\nt\n1\nn\n\n\n\nw\n" | fdisk $BLOCK_DEVICE
 
-mkfs.fat -F32 /dev/nvme0n1p1
-mkfs.btrfs -L Arch\ Linux /dev/nvme0n1p2
+mkfs.fat -F32 ${BLOCK_SUB}1
+mkfs.btrfs -f ${BLOCK_SUB}2 -L Arch\ Linux
 
 systemctl start reflector.service
 
